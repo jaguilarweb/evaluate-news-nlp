7805470462641702136ab7a7b8f5b1b16e9ad6cd
Jenny Aguilar <contact@jaguilarweb.com>
2021-04-28 20:09:08 -0400

    style: Add initials styles with scss files and style-loader settings

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,14 @@
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
 console.log(checkForName);
 
-alert("Hello you vvv ")
-console.log("CHANGE!");
+alert("Hello you ")
+console.log("CHANGE444!");
+document.getElementById('results').innerHTML = 'This a new result';
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
 
 module.exports = {
     entry: './src/client/index.js',
@@ -21,6 +21,10 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
@@ -29,14 +33,6 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
-        })
+
     ]
 }
